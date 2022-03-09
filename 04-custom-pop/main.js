cbe0418354d608bba5c05722eb5787a2ff016ae9
Edgar Benzor <edgarbenz@hotmail.com>
2022-03-08 18:59:54 -0600

    quite el script del html y lo lleve al archivo js

--- 04-custom-pop/main.js
+++ 04-custom-pop/main.js
@@ -5,22 +5,11 @@ document.body.appendChild(node);
 class NuevaTag extends HTMLElement {
     constructor() {
         super();
+        this.close = this.close.bind(this);
     }
 
     static get observedAttributes() { //what attributes will be observed
         return ['open'];  // We will work with "Open" attribute
-    }
-
-    get open() {
-        return this.hasAttribute('open');
-    }
-
-    set open(estaAbierto) {
-        if(estaAbierto) {
-            this.setAttribute('open', true)
-        } else {
-            this.removeAttribute('open');
-        }
     }
 
     attributesChangedCallback(attrName, oldValue, newValue) {
@@ -30,8 +19,52 @@ class NuevaTag extends HTMLElement {
     }
 
     connectedCallback() {
-        this.innerHTML = '<p>Edgariuse'
-    }
-}
+        const template = document.getElementById('plantilla');
+        const node =document.importNode(template.content, true);
+        this.appendChild(node);
 
-customElements.define('edgari-use', NuevaTag);
+        this.querySelector('button').addEventListener('click', this.close)
+
+
+        //this.innerHTML = '<p>Edgariuse'
+    }
+
+    get open() {
+        return this.hasAttribute('open');
+    }
+
+    set open(isOpen) {
+        this.querySelector('.wrapper').classList.toggle('open', isOpen);
+        if(isOpen) {
+            this.setAttribute('open', true)
+        } else {
+            this.removeAttribute('open');
+            //this.close();
+        }
+    }
+
+    close() {
+        console.log('el valor de this en el boton close es: ',this);
+        if(this.open !== false) {
+            this.open = false;
+        }
+    }
+
+}
+if(!customElements.get('edgar-popup')) {
+
+}
+customElements.define('edgar-popup', NuevaTag);
+
+
+const button = document.getElementById('otroBoton');
+button.addEventListener('click', (e) => {
+    const popup = document.getElementById('mipopup');
+    popup.open = true;
+
+
+    //const wrapper = document.querySelector('.wrapper');
+    //const hadFocus =document.activeElement;//who was the active element, ? who had the focus?
+    //wrapper.classList.add('open');
+    //botonCerrar.focus(); // I give focus to botonCerrar
+})
